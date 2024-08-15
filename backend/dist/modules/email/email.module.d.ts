da9aee115197a281dc76c5121eb14e205328a860
Ihab E. McShea <ihabshea@gmail.com>
2024-08-15 22:20:22 +0300

    add build settings to ci/cd

--- backend/dist/modules/email/email.module.d.ts
+++ backend/dist/modules/email/email.module.d.ts
@@ -1,2 +1,1 @@
-export declare class EmailModule {
-}
+export declare class EmailModule {}
--- backend/dist/modules/users/users.controller.js
+++ backend/dist/modules/users/users.controller.js
@@ -1,130 +1,163 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+'use strict';
+var __decorate =
+  (this && this.__decorate) ||
+  function (decorators, target, key, desc) {
+    var c = arguments.length,
+      r = c < 3 ? target : desc === null ? (desc = Object.getOwnPropertyDescriptor(target, key)) : desc,
+      d;
+    if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
+      r = Reflect.decorate(decorators, target, key, desc);
+    else
+      for (var i = decorators.length - 1; i >= 0; i--)
+        if ((d = decorators[i])) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+  };
+var __metadata =
+  (this && this.__metadata) ||
+  function (k, v) {
+    if (typeof Reflect === 'object' && typeof Reflect.metadata === 'function') return Reflect.metadata(k, v);
+  };
+var __param =
+  (this && this.__param) ||
+  function (paramIndex, decorator) {
+    return function (target, key) {
+      decorator(target, key, paramIndex);
+    };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.UsersController = void 0;
-const common_1 = require("@nestjs/common");
-const swagger_1 = require("@nestjs/swagger");
-const roles_decorator_1 = require("../../common/decorators/roles.decorator");
-const roles_guard_1 = require("../../common/guards/roles.guard");
-const jwt_auth_guard_1 = require("../auth/jwt-auth.guard");
-const paginatedUser_dto_1 = require("./dto/paginatedUser.dto");
-const updateProfile_dto_1 = require("./dto/updateProfile.dto");
-const user_dto_1 = require("./dto/user.dto");
-const userCreate_dto_1 = require("./dto/userCreate.dto");
-const userUpdate_dto_1 = require("./dto/userUpdate.dto");
-const users_service_1 = require("./users.service");
+const common_1 = require('@nestjs/common');
+const swagger_1 = require('@nestjs/swagger');
+const roles_decorator_1 = require('../../common/decorators/roles.decorator');
+const roles_guard_1 = require('../../common/guards/roles.guard');
+const jwt_auth_guard_1 = require('../auth/jwt-auth.guard');
+const paginatedUser_dto_1 = require('./dto/paginatedUser.dto');
+const updateProfile_dto_1 = require('./dto/updateProfile.dto');
+const user_dto_1 = require('./dto/user.dto');
+const userCreate_dto_1 = require('./dto/userCreate.dto');
+const userUpdate_dto_1 = require('./dto/userUpdate.dto');
+const users_service_1 = require('./users.service');
 let UsersController = class UsersController {
-    constructor(usersService) {
-        this.usersService = usersService;
+  constructor(usersService) {
+    this.usersService = usersService;
+  }
+  async getProfile(req) {
+    try {
+      const userId = req.user.userId;
+      const user = await this.usersService.findOne(userId);
+      if (!user) {
+        throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
+      }
+      return new user_dto_1.UserDto(user);
+    } catch (error) {
+      console.error('Error retrieving user:', error);
+      throw new common_1.HttpException(
+        'Unable to retrieve user. Please check the user ID and try again.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async getProfile(req) {
-        try {
-            const userId = req.user.userId;
-            const user = await this.usersService.findOne(userId);
-            if (!user) {
-                throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
-            }
-            return new user_dto_1.UserDto(user);
-        }
-        catch (error) {
-            console.error('Error retrieving user:', error);
-            throw new common_1.HttpException('Unable to retrieve user. Please check the user ID and try again.', common_1.HttpStatus.BAD_REQUEST);
-        }
+  }
+  async updateProfile(req, data) {
+    try {
+      const userId = req.user.userId;
+      const user = await this.usersService.updateUser(userId, data);
+      if (!user) {
+        throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
+      }
+      return new user_dto_1.UserDto(user);
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      throw new common_1.HttpException(
+        'Unable to update profile. Please check the input data and try again.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async updateProfile(req, data) {
-        try {
-            const userId = req.user.userId;
-            const user = await this.usersService.updateUser(userId, data);
-            if (!user) {
-                throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
-            }
-            return new user_dto_1.UserDto(user);
-        }
-        catch (error) {
-            console.error('Error updating profile:', error);
-            throw new common_1.HttpException('Unable to update profile. Please check the input data and try again.', common_1.HttpStatus.BAD_REQUEST);
-        }
+  }
+  async createUser(data) {
+    try {
+      const user = await this.usersService.createUser(data);
+      return new user_dto_1.UserDto(user);
+    } catch (error) {
+      throw new common_1.HttpException(
+        'Unable to create user. Please check the input data and try again.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async createUser(data) {
-        try {
-            const user = await this.usersService.createUser(data);
-            return new user_dto_1.UserDto(user);
-        }
-        catch (error) {
-            throw new common_1.HttpException('Unable to create user. Please check the input data and try again.', common_1.HttpStatus.BAD_REQUEST);
-        }
+  }
+  async findAll(page = 1, limit = 10) {
+    try {
+      return await this.usersService.findAll(page, limit);
+    } catch (error) {
+      console.error('Error retrieving users:', error);
+      throw new common_1.HttpException(
+        'Unable to retrieve users. Please try again later.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async findAll(page = 1, limit = 10) {
-        try {
-            return await this.usersService.findAll(page, limit);
-        }
-        catch (error) {
-            console.error('Error retrieving users:', error);
-            throw new common_1.HttpException('Unable to retrieve users. Please try again later.', common_1.HttpStatus.BAD_REQUEST);
-        }
+  }
+  async findOne(id) {
+    try {
+      const user = await this.usersService.findOne(id);
+      if (!user) {
+        throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
+      }
+      return new user_dto_1.UserDto(user);
+    } catch (error) {
+      console.error('Error retrieving user:', error);
+      throw new common_1.HttpException(
+        'Unable to retrieve user. Please check the user ID and try again.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async findOne(id) {
-        try {
-            const user = await this.usersService.findOne(id);
-            if (!user) {
-                throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
-            }
-            return new user_dto_1.UserDto(user);
-        }
-        catch (error) {
-            console.error('Error retrieving user:', error);
-            throw new common_1.HttpException('Unable to retrieve user. Please check the user ID and try again.', common_1.HttpStatus.BAD_REQUEST);
-        }
+  }
+  async updateUser(id, data) {
+    try {
+      const user = await this.usersService.updateUser(id, data);
+      if (!user) {
+        throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
+      }
+      return new user_dto_1.UserDto(user);
+    } catch (error) {
+      console.error('Error updating user:', error);
+      throw new common_1.HttpException(
+        'Unable to update user. Please check the input data and try again.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async updateUser(id, data) {
-        try {
-            const user = await this.usersService.updateUser(id, data);
-            if (!user) {
-                throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
-            }
-            return new user_dto_1.UserDto(user);
-        }
-        catch (error) {
-            console.error('Error updating user:', error);
-            throw new common_1.HttpException('Unable to update user. Please check the input data and try again.', common_1.HttpStatus.BAD_REQUEST);
-        }
+  }
+  async deleteUser(id) {
+    try {
+      const user = await this.usersService.deleteUser(id);
+      if (!user) {
+        throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
+      }
+      return new user_dto_1.UserDto(user);
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      throw new common_1.HttpException(
+        'Unable to delete user. Please try again later.',
+        common_1.HttpStatus.BAD_REQUEST,
+      );
     }
-    async deleteUser(id) {
-        try {
-            const user = await this.usersService.deleteUser(id);
-            if (!user) {
-                throw new common_1.HttpException('User not found', common_1.HttpStatus.NOT_FOUND);
-            }
-            return new user_dto_1.UserDto(user);
-        }
-        catch (error) {
-            console.error('Error deleting user:', error);
-            throw new common_1.HttpException('Unable to delete user. Please try again later.', common_1.HttpStatus.BAD_REQUEST);
-        }
-    }
+  }
 };
 exports.UsersController = UsersController;
-__decorate([
+__decorate(
+  [
     (0, common_1.Get)('profile'),
     (0, swagger_1.ApiResponse)({ status: 200, description: 'User details', type: user_dto_1.UserDto }),
     (0, swagger_1.ApiResponse)({ status: 404, description: 'User not found' }),
     __param(0, (0, common_1.Req)()),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Object]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "getProfile", null);
-__decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [Object]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'getProfile',
+  null,
+);
+__decorate(
+  [
     (0, common_1.Put)('profile'),
     (0, swagger_1.ApiBody)({ type: userUpdate_dto_1.UpdateUserDto }),
     (0, swagger_1.ApiResponse)({ status: 200, description: 'User updated successfully', type: user_dto_1.UserDto }),
@@ -132,44 +165,74 @@ __decorate([
     (0, swagger_1.ApiResponse)({ status: 404, description: 'User not found' }),
     __param(0, (0, common_1.Req)()),
     __param(1, (0, common_1.Body)()),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Object, updateProfile_dto_1.UpdateProfileDto]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "updateProfile", null);
-__decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [Object, updateProfile_dto_1.UpdateProfileDto]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'updateProfile',
+  null,
+);
+__decorate(
+  [
     (0, roles_decorator_1.Roles)('admin'),
     (0, common_1.Post)(),
     (0, swagger_1.ApiBody)({ type: userCreate_dto_1.UserCreateDto }),
     (0, swagger_1.ApiResponse)({ status: 201, description: 'User created successfully', type: user_dto_1.UserDto }),
     (0, swagger_1.ApiResponse)({ status: 400, description: 'Bad Request' }),
     __param(0, (0, common_1.Body)()),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [userCreate_dto_1.UserCreateDto]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "createUser", null);
-__decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [userCreate_dto_1.UserCreateDto]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'createUser',
+  null,
+);
+__decorate(
+  [
     (0, roles_decorator_1.Roles)('admin'),
     (0, common_1.Get)(),
     (0, swagger_1.ApiQuery)({ name: 'page', required: false, type: Number, description: 'Page number', example: 1 }),
-    (0, swagger_1.ApiQuery)({ name: 'limit', required: false, type: Number, description: 'Number of results per page', example: 10 }),
-    (0, swagger_1.ApiResponse)({ status: 200, description: 'List of users', type: paginatedUser_dto_1.PaginatedUserDto }),
+    (0, swagger_1.ApiQuery)({
+      name: 'limit',
+      required: false,
+      type: Number,
+      description: 'Number of results per page',
+      example: 10,
+    }),
+    (0, swagger_1.ApiResponse)({
+      status: 200,
+      description: 'List of users',
+      type: paginatedUser_dto_1.PaginatedUserDto,
+    }),
     (0, swagger_1.ApiResponse)({ status: 400, description: 'Bad Request' }),
     __param(0, (0, common_1.Query)('page', common_1.ParseIntPipe)),
     __param(1, (0, common_1.Query)('limit', common_1.ParseIntPipe)),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number, Number]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "findAll", null);
-__decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [Number, Number]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'findAll',
+  null,
+);
+__decorate(
+  [
     (0, common_1.Get)(':id'),
     (0, swagger_1.ApiResponse)({ status: 200, description: 'User details', type: user_dto_1.UserDto }),
     (0, swagger_1.ApiResponse)({ status: 404, description: 'User not found' }),
     __param(0, (0, common_1.Param)('id', common_1.ParseIntPipe)),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "findOne", null);
-__decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [Number]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'findOne',
+  null,
+);
+__decorate(
+  [
     (0, roles_decorator_1.Roles)('admin'),
     (0, common_1.Put)(':id'),
     (0, swagger_1.ApiBody)({ type: userUpdate_dto_1.UpdateUserDto }),
@@ -178,25 +241,37 @@ __decorate([
     (0, swagger_1.ApiResponse)({ status: 404, description: 'User not found' }),
     __param(0, (0, common_1.Param)('id', common_1.ParseIntPipe)),
     __param(1, (0, common_1.Body)()),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number, userUpdate_dto_1.UpdateUserDto]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "updateUser", null);
-__decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [Number, userUpdate_dto_1.UpdateUserDto]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'updateUser',
+  null,
+);
+__decorate(
+  [
     (0, roles_decorator_1.Roles)('admin'),
     (0, common_1.Delete)('user/:id'),
     (0, swagger_1.ApiResponse)({ status: 200, description: 'User deleted successfully', type: user_dto_1.UserDto }),
     (0, swagger_1.ApiResponse)({ status: 404, description: 'User not found' }),
     __param(0, (0, common_1.Param)('id', common_1.ParseIntPipe)),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number]),
-    __metadata("design:returntype", Promise)
-], UsersController.prototype, "deleteUser", null);
-exports.UsersController = UsersController = __decorate([
+    __metadata('design:type', Function),
+    __metadata('design:paramtypes', [Number]),
+    __metadata('design:returntype', Promise),
+  ],
+  UsersController.prototype,
+  'deleteUser',
+  null,
+);
+exports.UsersController = UsersController = __decorate(
+  [
     (0, common_1.Controller)('users'),
     (0, swagger_1.ApiTags)('users'),
     (0, swagger_1.ApiBearerAuth)(),
     (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard, roles_guard_1.RolesGuard),
-    __metadata("design:paramtypes", [users_service_1.UsersService])
-], UsersController);
+    __metadata('design:paramtypes', [users_service_1.UsersService]),
+  ],
+  UsersController,
+);
 //# sourceMappingURL=users.controller.js.map
