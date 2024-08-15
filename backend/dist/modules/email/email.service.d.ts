4c1b5b65c544ff2fddf3dd7b858ad1b8603ea475
Ihab E. McShea <ihabshea@gmail.com>
2024-08-15 21:12:49 +0300

    update ci/cd

--- backend/dist/modules/email/email.service.d.ts
+++ backend/dist/modules/email/email.service.d.ts
@@ -3,10 +3,10 @@ import { ConfigService } from '@nestjs/config';
 import { MailerService } from '@nestjs-modules/mailer';
 import { RedisService } from '../redis/redis.service';
 export declare class EmailService implements OnModuleInit {
-  private readonly redisService;
-  private readonly mailerService;
-  private readonly configService;
-  constructor(redisService: RedisService, mailerService: MailerService, configService: ConfigService);
-  onModuleInit(): Promise<void>;
-  handleEmailVerification(message: string): Promise<void>;
+    private readonly redisService;
+    private readonly mailerService;
+    private readonly configService;
+    constructor(redisService: RedisService, mailerService: MailerService, configService: ConfigService);
+    onModuleInit(): Promise<void>;
+    handleEmailVerification(message: string): Promise<void>;
 }
--- backend/dist/modules/users/users.controller.d.ts
+++ backend/dist/modules/users/users.controller.d.ts
@@ -1,17 +1,17 @@
 import { PaginatedUserDto } from './dto/paginatedUser.dto';
+import { UpdateProfileDto } from './dto/updateProfile.dto';
 import { UserDto } from './dto/user.dto';
 import { UserCreateDto } from './dto/userCreate.dto';
 import { UpdateUserDto } from './dto/userUpdate.dto';
 import { UsersService } from './users.service';
-import { UpdateProfileDto } from './dto/updateProfile.dto';
 export declare class UsersController {
-  private readonly usersService;
-  constructor(usersService: UsersService);
-  getProfile(req: any): Promise<UserDto>;
-  updateProfile(req: any, data: UpdateProfileDto): Promise<UserDto>;
-  createUser(data: UserCreateDto): Promise<UserDto>;
-  findAll(page?: number, limit?: number): Promise<PaginatedUserDto>;
-  findOne(id: number): Promise<UserDto>;
-  updateUser(id: number, data: UpdateUserDto): Promise<UserDto>;
-  deleteUser(id: number): Promise<UserDto>;
+    private readonly usersService;
+    constructor(usersService: UsersService);
+    getProfile(req: any): Promise<UserDto>;
+    updateProfile(req: any, data: UpdateProfileDto): Promise<UserDto>;
+    createUser(data: UserCreateDto): Promise<UserDto>;
+    findAll(page?: number, limit?: number): Promise<PaginatedUserDto>;
+    findOne(id: number): Promise<UserDto>;
+    updateUser(id: number, data: UpdateUserDto): Promise<UserDto>;
+    deleteUser(id: number): Promise<UserDto>;
 }
